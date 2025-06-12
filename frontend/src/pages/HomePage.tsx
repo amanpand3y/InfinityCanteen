6b50b4d97384c227095f8f06b111bd5ae069e28a
amanpand3y <aman.20234186@mnnit.ac.in>
2025-06-13 04:22:51 +0530

    created Home Page

---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,30 @@
+import landingImage from "../assets/landing.png"
+import appDownload from "../assets/appDownload (1).png"
+
+const HomePage = () => {
+    return(
+        <div className="flex flex-col gap-12">
+            <div className="bg-gradient-to-tr from-blue-900 via-cyan-900 to-gray-900 text-white rounded-lg shadow-md py-8  flex flex-col gap-5 text-center -mt-16">
+                <h1 className="text-5xl font-bold tracking-tight text-orange-600">
+                    Tuck into a takeaway today
+                </h1>
+                <span className="text-xl">Food is just a click away</span>
+            </div>
+
+            <div className="grid md:grid-cols-2 gap-5">
+                <img src={landingImage}/>
+                <div className="flex flex-col items-center justify-center gap-4 text-center">
+                    <span className="font-bold text-3xl tracking-tighter">
+                        Order takeaway even faster!
+                    </span>
+                    <span>
+                        Download the InfinityCanteen App for faster ordering and personalised recommendations
+                    </span>
+                    <img src={appDownload}/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HomePage
