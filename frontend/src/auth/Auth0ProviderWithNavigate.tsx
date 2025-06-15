5d0edc469ebcd484253cea02d767f7bfbc66012b
amanpand3y <aman.20234186@mnnit.ac.in>
2025-06-15 23:12:15 +0530

    Backend access only with access token

--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,10 +1,5 @@
-import { useCreateMyUser } from "@/api/MyUserApi";
-import { Auth0Provider, User } from "@auth0/auth0-react";
+import { Auth0Provider} from "@auth0/auth0-react";
 import { useNavigate } from "react-router-dom";
-
-type AppState = {
-  returnTo?: string;
-};
 
 
 type Props = {
@@ -16,12 +11,13 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
+  const audience=import.meta.env.VITE_AUTH0_AUDIENCE;
 
-  if (!domain || !clientId || !redirectUri ) {
+  if (!domain || !clientId || !redirectUri ||!audience ) {
     throw new Error("unable to initialise auth");
   }
 
-  const onRedirectCallback = (appState?: AppState, user?: User) => {
+  const onRedirectCallback = () => {
     navigate("/auth-callback");
     
   };
@@ -32,6 +28,7 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
       clientId={clientId}
       authorizationParams={{
         redirect_uri: redirectUri,
+        audience,
       }}
       onRedirectCallback={onRedirectCallback}
     >
