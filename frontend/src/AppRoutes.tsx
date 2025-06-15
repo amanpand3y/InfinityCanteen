70c58dfda74acab80320cc116159d34508a9ab8d
amanpand3y <aman.20234186@mnnit.ac.in>
2025-06-15 22:32:38 +0530

    set up access token

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,11 +1,13 @@
 import Layout from "./layouts/layout.tsx"
 import { Navigate, Route,Routes } from "react-router-dom"
 import HomePage from "./pages/HomePage.tsx"
+import AuthCallbackPage from "./pages/AuthCallbackPage.tsx";
 
 const AppRoutes = ()=>{
     return(
         <Routes>
             <Route path="/" element={<Layout><HomePage/></Layout>}/>
+            <Route path="/auth-callback" element ={<AuthCallbackPage/>}/>
             <Route path="/user-profile" element={<span>USER PROFILE PAGE</span>}/>
             <Route path="*" element={<Navigate to="/"/>}/>
         </Routes>
--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,5 +1,6 @@
 import { useCreateMyUser } from "@/api/MyUserApi";
 import { Auth0Provider, User } from "@auth0/auth0-react";
+import { useNavigate } from "react-router-dom";
 
 type AppState = {
   returnTo?: string;
@@ -11,7 +12,7 @@ type Props = {
 };
 
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
-  const {createUser} = useCreateMyUser();
+  const navigate = useNavigate();
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
@@ -21,9 +22,8 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
   }
 
   const onRedirectCallback = (appState?: AppState, user?: User) => {
-    if(user?.sub && user?.email){
-      createUser({auth0Id: user.sub,email:user.email});
-    }
+    navigate("/auth-callback");
+    
   };
  
   return (
